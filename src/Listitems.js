9b74b72b92389047ae67b90cd1a21398601a8707
Mark Chukwuebuka <markedah9652@gmail.com>
2020-09-16 14:38:08 +0100

    3rd commit

--- src/Listitems.js
+++ src/Listitems.js
@@ -6,10 +6,20 @@ function ListItems(props){
     const items = props.items;
     const listitems = items.map(item =>
         {
-            return <div className="list" key = "item.key">
-                <p>{item.text}
+            return <div className="list" key = { item.key }>
+                <p>
+                    <input type = "text" 
+                    id={ item.key } 
+                    value = { item.text }
+                    onChange = {
+                        (e) => {
+                            props.setUpdate(e.target.value, item.key)
+                        }
+                    }
+                    />
                 <span>
-                    <FontAwesomeIcon className="faicons" icon = 'trash'/>
+                    <FontAwesomeIcon className="faicons" icon = 'trash' onClick={ () => props.deleteItem(item.key)
+                    }/>
                 </span>
                 </p>
             </div>
