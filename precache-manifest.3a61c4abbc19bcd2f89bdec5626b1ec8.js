87629d808ecb14312380f2f92244359c3c247597
Mark Chukwuebuka <markedah9652@gmail.com>
2020-09-17 11:38:18 +0100

    Updates

---
+++ precache-manifest.3a61c4abbc19bcd2f89bdec5626b1ec8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68f0567a5983cd4492656dfa99cc5c97",
+    "url": "/React-ToDo-app/index.html"
+  },
+  {
+    "revision": "911a3729d29f062ab371",
+    "url": "/React-ToDo-app/static/css/main.a5657b9a.chunk.css"
+  },
+  {
+    "revision": "960a3d3d33e09244908c",
+    "url": "/React-ToDo-app/static/js/2.d0a8831a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDo-app/static/js/2.d0a8831a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "911a3729d29f062ab371",
+    "url": "/React-ToDo-app/static/js/main.5ab302e2.chunk.js"
+  },
+  {
+    "revision": "1c53add9e65e70fe7bb6",
+    "url": "/React-ToDo-app/static/js/runtime-main.015d324b.js"
+  }
+]);
